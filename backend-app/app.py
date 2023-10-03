6b89079ab7d92bb2b4c3efac1ea9609c9099b684
Shreyanand <shanand@redhat.com>
2023-10-03 11:15:27 -0400

    Add gradio app

--- backend-app/app.py
+++ backend-app/app.py
@@ -1,25 +1,40 @@
 from document_retrieval import DocumentRetrieval
 from answer_generation import PROMPT_TEMPLATE, AnswerGeneration
+import gradio as gr
 import argparse
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Document search and answer generation")
-    parser.add_argument("--query", required=True, help="Enter your query about the ROSA service")
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(description="Document search and answer generation")
+    # parser.add_argument("--query", required=True, help="Enter your query about the ROSA service")
+    # args = parser.parse_args()
+
+    def answer(query):
+            config_path = './config.json'
+            retrieval = DocumentRetrieval(config_path)
+            top_candidates = retrieval.search_candidates(query)
+            print("Top Candidates:")
+            for idx, candidate in enumerate(top_candidates):
+                print(f"{idx + 1}: {candidate}")
+            generation = AnswerGeneration(retrieval.vector_store,
+                                          'text-davinci-003')
+            answer = generation.generate(query,
+                                top_candidates,
+                                PROMPT_TEMPLATE)
+            print(f"Final answer: \n {answer}")
+            return answer
     
-    config_path = 'config.json'
-    retrieval = DocumentRetrieval(config_path)
-    top_candidates = retrieval.search_candidates(args.query)
-    print("Top Candidates:")
-    for idx, candidate in enumerate(top_candidates):
-        print(f"{idx + 1}: {candidate}")
+    #answer("What is ROSA?")
         
-    generation = AnswerGeneration(retrieval.vector_store,
-                                  'text-davinci-003')
+    demo = gr.Interface(fn=answer,
+                        inputs="text",
+                        outputs="text")
+    
+    demo.launch()   
+
+
         
-    answer = generation.generate(args.query,
-                                 top_candidates,
-                                 PROMPT_TEMPLATE)
-    print(f"Final answer: \n {answer}")
+
+        
+
         
     
